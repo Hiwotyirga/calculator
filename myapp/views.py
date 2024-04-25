0d7804314cfe13325194328bc8c10643681e14dc
Hiwot yirga <hiwotyirga30@gmail.com>
2024-04-25 19:25:59 +0300

    second commit

--- myapp/views.py
+++ myapp/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse,JsonResponse
 # Create your views here.
 
 def function (request):
-    # return render(request,"index.html")
-    return HttpResponse("server start")
+    return render(request,"index.html")
+    
