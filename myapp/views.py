361cf916f06654ef32404757686ab782b0c45033
Hiwot yirga <hiwotyirga30@gmail.com>
2024-04-26 13:51:57 +0300

    forth commit

--- myapp/views.py
+++ myapp/views.py
@@ -7,12 +7,25 @@ def function (request):
     return render(request,"index.html")
 
 def submitquery1(request):
-    print(request.GET)  # Check the contents of GET parameters in the console
+    q = request.GET["query"]
     try:
-        q = request.GET["query"]
-        return HttpResponse(q)
-    except KeyError:
-        return HttpResponse("Query parameter is missing", status=400)
+        ans=eval(q)
+        dictionary={
+            "q":q,
+            "ans":ans,
+            "error":False
+        }
+        return render (request, "index.html",context=dictionary)
+    except Exception as e:
+        dictionary={
+            "error-message":str(e),
+            "error":True
+        }
+        return render (request,"index.html",context=dictionary)
+        
+     
+
+    
 
 
 
